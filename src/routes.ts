2f7d0dd8e0432561a514ae66ca4e8231238fc05d
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-10-16 00:13:50 -0300

    Adds show orphanage logic

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import orphanagesController from './controllers/orphanages-controller';
 const routes = Router();
 
 routes.get('/orphanages', orphanagesController.index);
+routes.get('/orphanages/:id', orphanagesController.show);
 routes.post('/orphanages', orphanagesController.create);
 
 export default routes;
