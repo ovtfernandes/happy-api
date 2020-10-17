f4f50ba00d294181fbf4aff86c1e3712aef34ed1
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-10-17 17:54:06 -0300

    Adds postgres connection

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,3 +1,21 @@
-import { createConnection } from 'typeorm';
+import { ConnectionOptions, createConnection } from 'typeorm';
+import path from 'path';
 
-createConnection();
+const url = process.env.DATABASE_URL;
+
+const config = {
+    type: url ? 'postgres' : 'sqlite',
+    database: url ? undefined : path.join(__dirname, 'database.sqlite'),
+    url,
+    migrations: [
+        path.join(__dirname, 'migrations', '*.ts'),
+    ],
+    entities: [
+        path.join(__dirname, '..', 'models', '*.ts'),
+    ],
+    cli: {
+        migrationsDir: path.join(__dirname, 'migrations'),
+    }
+};
+
+createConnection(config as ConnectionOptions);
